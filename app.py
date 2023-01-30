b1d684f2d0a4976c79a96c00e9c9664ae7bba468
mesh05 <90903383+mesh05@users.noreply.github.com>
2023-01-30 22:30:52 +0530

    semi working login

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template, request
 
+
 app = Flask(__name__)
+
+
+USERS = {"prathamesh": "pratham"}
 
 
 @app.route("/")
@@ -8,14 +12,36 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/greet", methods=["POST"])
+@app.route("/welcome", methods=["GET", "POST"])
+def welcome():
+    if request.method == 'POST':
+        username = request.form.get("user")
+        password = request.form.get("pass")
+    if (USERS[username] == password):
+        return "hello"
+    else:
+        return "Failed login"
+
+
+@app.route("/Login", methods=["GET", "POST"])
+def login():
+
+    return render_template("Login.html")
+
+
+@app.route("/contact", methods=["GET", "POST"])
+def contact():
+    return render_template("contact.html")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+'''@app.route("/greet", methods=["POST"])
 def greet():
     name = request.form.get("name", "world")
     if name == "shoaib":
         return render_template("greet.html", name="chutiye")
     else:
-        return render_template("greet.html", name=name)
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
+        return render_template("greet.html", name=name)'''
