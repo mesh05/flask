011d043bddcf25c2336903951e5ba857494716ab
mesh05 <90903383+mesh05@users.noreply.github.com>
2023-02-12 18:36:28 +0530

    v1.0

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
 from flask import Flask, render_template, request
+from flask_mysqldb import MySQL
 
 
 app = Flask(__name__)
 
 
-USERS = {"prathamesh": "pratham"}
+# MySQL configurations
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = '22EC1U3Inisl#wiswAPe'
+app.config['MYSQL_DB'] = 'emsdb'
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+
+
+mysql = MySQL(app)
+
+
+USERS = {"prathamesh": "pratham", "Shoaib": "root"}
 
 
 @app.route("/")
@@ -14,16 +26,42 @@ def index():
 
 @app.route("/welcome", methods=["GET", "POST"])
 def welcome():
+    cur = mysql.connection.cursor()
+    cur.execute('''call ac_det''')
+    acc_det = cur.fetchall()
+    cur.close()
     if request.method == 'POST':
         username = request.form.get("user")
         password = request.form.get("pass")
-    if (USERS[username] == password):
-        return "hello"
-    else:
-        return "Failed login"
+
+    cur = mysql.connection.cursor()
+    cur.callproc('staff_timetable', [int(username)])
+    det = cur.fetchall()
+    cur.close()
+
+    x = len(det)
+    value = []
+    for i in acc_det:
+        if (i['staff_id'] == int(username) and i['staff_pass'] == password):
+            cur = mysql.connection.cursor()
+            cur.callproc('staff_details', [int(username)])
+            staff = cur.fetchall()
+            cur.close()
+            age = staff[0]['age']
+            dept = staff[0]['dept_name']
+            name = i['staff_name']
+            st_id = staff[0]['staff_id']
+            desg = staff[0]['designation']
+            key = det[1].keys()
+            for j in range(0, x):
+                row = []
+                for k in key:
+                    row.append(det[j][k])
+                value.append(row)
+    return render_template("table.html", name=name, key=key, value=value, dept=dept, id=st_id, desg=desg, age=age)
 
 
-@app.route("/Login", methods=["GET", "POST"])
+@app.route("/login", methods=["GET", "POST"])
 def login():
 
     return render_template("Login.html")
@@ -32,6 +70,12 @@ def login():
 @app.route("/contact", methods=["GET", "POST"])
 def contact():
     return render_template("contact.html")
+
+
+@app.route("/details", methods=["GET", "POST"])
+def details():
+    cur = mysql.connection.cursor()
+    return 'done'
 
 
 if __name__ == '__main__':
