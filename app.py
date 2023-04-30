86a92bc6c54779b178de2917a678dca46af5e2ca
mesh05 <90903383+mesh05@users.noreply.github.com>
2023-04-30 19:17:30 +0530

    new app.py

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, send_file
 from flask_mysqldb import MySQL
+import io
+import pandas as pd
+import xlsxwriter
+from datetime import date
 
+today = date.today()
 
 app = Flask(__name__)
 
@@ -14,9 +19,6 @@ app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 
 
 mysql = MySQL(app)
-
-
-USERS = {"prathamesh": "pratham", "Shoaib": "root"}
 
 
 @app.route("/")
@@ -47,18 +49,18 @@ def welcome():
             cur.callproc('staff_details', [int(username)])
             staff = cur.fetchall()
             cur.close()
-            age = staff[0]['age']
-            dept = staff[0]['dept_name']
+            # age = staff[0]['age']
+            # dept = staff[0]['dept_name']
             name = i['staff_name']
-            st_id = staff[0]['staff_id']
-            desg = staff[0]['designation']
+            # st_id = staff[0]['staff_id']
+            # desg = staff[0]['designation']
             key = det[1].keys()
             for j in range(0, x):
                 row = []
                 for k in key:
                     row.append(det[j][k])
                 value.append(row)
-            return render_template("welcome.html")
+            return render_template("welcome.html", name=name)
             # return render_template("table.html", name=name, key=key, value=value, dept=dept, id=st_id, desg=desg, age=age)
     return "login failed!!"
 
@@ -71,13 +73,66 @@ def login():
 
 @app.route("/contact", methods=["GET", "POST"])
 def contact():
-    return render_template("contact.html")
+    name = request.form.get("name")
+    mail = request.form.get("mail")
+    message = request.form.get("message")
+    cur = mysql.connection.cursor()
+    cur.callproc('insert_contact', [name, mail, message])
+    cur.close()
+    return render_template("contact.html", name=name, message=message, mail=mail)
 
 
 @app.route("/details", methods=["GET", "POST"])
 def details():
-    cur = mysql.connection.cursor()
-    return 'done'
+    day = request.form.get("day")
+    start = int(request.form.get("start"))
+    end = int(request.form.get("end"))
+    hall = int(request.form.get("hall"))
+    # file = request.files['file']
+    # file.save(file.filename)
+    # Parse the data as a Pandas DataFrame type
+    # data = ((pd.read_excel(file))['Halls']).tolist()
+    # print(data)
+    if start < end:
+        cur = mysql.connection.cursor()
+        cur.callproc('final_list', [day])
+        staff = cur.fetchall()
+        print(staff)
+        x = len(staff)
+        cur.close()
+        key = staff[1].keys()
+        value = []
+        for j in range(0, x):
+            row = []
+            for k in key:
+                row.append(staff[j][k])
+            value.append(row)
+        print(value)
+        count = 0
+        final_list = [['staff_id', 'staff_name','designation', 'dept_name', 'age']]
+        for i in value:
+            flag = 0
+            for j in range((4+start), ((4+end)+1)):
+                print(i[j])
+                if (i[j] != None):
+                    flag = 1
+                    break
+            if(count >= hall):
+                break
+            if (flag == 0):
+                final_list.append(i[0:5])
+                count = count+1
+        try:
+            df = pd.DataFrame(final_list[1:], columns=final_list[0])
+            print(df)
+            output = io.BytesIO()
+            writer = pd.ExcelWriter(output, engine='xlsxwriter')
+            df.to_excel(writer, sheet_name='Sheet1')
+            writer.save()
+            output.seek(0)
+            return send_file(output, mimetype='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', as_attachment=True, download_name='data.xlsx')
+        except IndexError:
+            return "NO INVIGILATORS FOUND!!!"
 
 
 if __name__ == '__main__':
